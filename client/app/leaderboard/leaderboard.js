98eb347d7f2243d6507c5c69ab19d58213744106
Michael Chow <mcmcmc426@gmail.com>
2016-09-07 13:09:26 -0700

    front-end updates with simple navigation added

--- client/app/leaderboard/leaderboard.js
+++ client/app/leaderboard/leaderboard.js
@@ -1,5 +1,17 @@
 angular.module('app.leaderboard', [])
 
-.controller('LeaderboardController', function ($scope) {
-  // Your code here
+.controller('LeaderboardController', function ($scope, $http) {
+  	
+  	$scope.leaderboard = [];
+  	
+  	$http({
+  		method: 'GET',
+  		url: //tbd
+  	})
+  	.then(function(leaderboard) {
+  		$scope.leaderboard = leaderboard.data;
+  	})
+  	.catch(function(err) {
+  		console.log('Error occured: ', err);
+  	});
 });

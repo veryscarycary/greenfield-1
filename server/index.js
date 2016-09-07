dbdf2795dd04626de9fd09ec6bea5c24dbab777f
Cary <carymeskell@gmail.com>
2016-09-06 19:27:47 -0700

    Add DB files

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var db = require('./db/index');
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/../client'));
 
-app.get('/', function(req,res) {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get('/', function(req, res) {
   res.redirect('/index.html');
 });
 

fc53050858a8ddfc923de25ee063c6e451175ab5
Cary <carymeskell@gmail.com>
2016-09-06 17:52:48 -0700

    Confirm render pages to screen with server

--- server/index.js
+++ server/index.js
@@ -5,8 +5,9 @@ var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/../client'));
 
-
-
+app.get('/', function(req,res) {
+  res.redirect('/index.html');
+});
 
 
 http.listen(3000, '127.0.0.1');

f90dbf82c1aac7da2f4bf65295bf5a4f676e8585
Cary Meskell <carymeskell@gmail.com>
2020-04-07 20:23:27 -0700

    update mongodb uri

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var mongoUri = 'mongodb://localhost/greenfield';
+var mongoUri = process.env.MONGODB_URI;
 
 // Connect Mongoose to our local MongoDB via URI specified above and export it below
 var db = mongoose.connect(mongoUri);

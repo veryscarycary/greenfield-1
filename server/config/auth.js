c7d8817d82bd110f3ded39adda27f345b9c35d6b
Cary Meskell <carymeskell@gmail.com>
2024-07-12 00:13:59 -0700

    fix ip for prod

--- server/config/auth.js
+++ server/config/auth.js
@@ -2,11 +2,19 @@
 var ip = require('./ip.js');
 var port = require('./port.js');
 
+var scheme = process.env.ENVIRONMENT === 'production' ? 'https' : 'http';
+var baseEndpoint =
+  process.env.ENVIRONMENT === 'production'
+    ? process.env.RAILWAY_PUBLIC_DOMAIN
+    : `${ip === '0.0.0.0' ? 'localhost' : ip}:${port}`;
+
+var callbackURL = `${scheme}://${baseEndpoint}/auth/facebook/callback`;
+
 module.exports = {
-  'facebookAuth': {
-    'clientID': '1174473392615797',
-    'clientSecret': 'b6f71776c158a48e6f3b04d8f1ebab50',
-    'callbackURL': `http://${ip === '0.0.0.0' ? 'localhost' : ip}:${port}/auth/facebook/callback`
+  facebookAuth: {
+    clientID: '1174473392615797',
+    clientSecret: 'b6f71776c158a48e6f3b04d8f1ebab50',
+    callbackURL: callbackURL,
     //tells fb when it responds, it's secure to send data back to this path
-  }
+  },
 };
--- server/config/ip.js
+++ server/config/ip.js
@@ -1,11 +1,5 @@
 // change depending on local or dev env; localhost or ....
-var ip;
-
-if (process.env.ENVIRONMENT === 'production') {
-  ip = process.env.RAILWAY_PUBLIC_DOMAIN;
-} else {
-  var DB_CONTAINER_NAME = process.env.DOCKER ? 'mongodb' : null;
-  ip = DB_CONTAINER_NAME ? '0.0.0.0' : 'localhost';
-}
+var DB_CONTAINER_NAME = process.env.DOCKER ? 'mongodb' : null;
+var ip = DB_CONTAINER_NAME ? '0.0.0.0' : 'localhost';
 
 module.exports = ip;

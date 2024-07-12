b6ce6cc87b5700839f17ecd8ec1d5175640b5fc9
Cary Meskell <carymeskell@gmail.com>
2024-07-11 21:34:34 -0700

    add mongo url env variable

--- server/db/index.js
+++ server/db/index.js
@@ -2,11 +2,11 @@ var mongoose = require('mongoose');
 
 
 var DB_CONTAINER_NAME = process.env.DOCKER ? 'mongodb' : null;
-var MONGO_URI = `mongodb://${DB_CONTAINER_NAME || 'localhost'}/greenfield`;
+var MONGO_URL = process.env.MONGO_URL || `mongodb://${DB_CONTAINER_NAME || 'localhost'}/greenfield`;
 
 // Connect Mongoose to our local MongoDB via URI specified above and export it below
 var db = mongoose
-  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Successfully connected to MongoDB'))
   .catch((err) => console.error('Connection error:', err));
 

a3c4577268e1af88e12a959f8ba57a2a3c036ba1
Cary Meskell <carymeskell@gmail.com>
2024-07-13 01:11:07 -0700

    last ditch effort

--- server/db/index.js
+++ server/db/index.js
@@ -18,11 +18,7 @@ console.log('MONGOHOST: ' + process.env.MONGO_HOST);
 console.log('hostname: ' + hostname);
 // Connect Mongoose to our local MongoDB via URI specified above and export it below
 var db = mongoose
-  .connect(mongoUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    family: 4 // use ipv4, skip trying ipv6
-  })
+  .connect(mongoUrl)
   .then(() => console.log('Successfully connected to MongoDB'))
   .catch((err) => {
     console.error('Connection error:', err);

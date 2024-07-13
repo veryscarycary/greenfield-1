09fa6b453710d5f19dc5cc947ace9ecbe054e3f8
Cary Meskell <carymeskell@gmail.com>
2024-07-12 17:10:55 -0700

    change mongo url for prod

--- server/db/index.js
+++ server/db/index.js
@@ -1,12 +1,16 @@
 var mongoose = require('mongoose');
 
-
-var DB_CONTAINER_NAME = process.env.DOCKER ? 'mongodb' : null;
-var MONGO_URL = process.env.MONGO_URL || `mongodb://${DB_CONTAINER_NAME || 'localhost'}/greenfield`;
+var hostname =
+  process.env.ENVIRONMENT === 'production'
+    ? process.env.RAILWAY_PRIVATE_DOMAIN
+    : process.env.DOCKER
+    ? 'mongodb'
+    : 'localhost';
+var mongoUrl = `mongodb://${hostname}/greenfield`;
 
 // Connect Mongoose to our local MongoDB via URI specified above and export it below
 var db = mongoose
-  .connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Successfully connected to MongoDB'))
   .catch((err) => console.error('Connection error:', err));
 

357a6cf0bc2fe744afdfc88247f379589611ba83
Cary Meskell <carymeskell@gmail.com>
2024-07-12 17:27:16 -0700

    console debugging

--- server/db/index.js
+++ server/db/index.js
@@ -2,12 +2,14 @@ var mongoose = require('mongoose');
 
 var hostname =
   process.env.ENVIRONMENT === 'production'
-    ? process.env.RAILWAY_PRIVATE_DOMAIN
+    ? process.env.MONGO_HOST
     : process.env.DOCKER
     ? 'mongodb'
     : 'localhost';
 var mongoUrl = `mongodb://${hostname}/greenfield`;
 
+console.log('MONGOHOST: ' + process.env.MONGO_HOST);
+console.log('hostname: ' + hostname);
 // Connect Mongoose to our local MongoDB via URI specified above and export it below
 var db = mongoose
   .connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })

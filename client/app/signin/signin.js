c2d15c97db335cd3d6147f6d3a1d951db1ccf3e9
Cary Meskell <carymeskell@gmail.com>
2024-08-05 23:45:13 -0700

    remove directives, because login and signup are no longer elements, just routes

--- client/app/signin/signin.js
+++ client/app/signin/signin.js
@@ -2,11 +2,4 @@ angular.module('app.signin', [])
 
 .controller('SigninController', function ($scope, $http, $location) {
 
-}).directive('signIn', function() {
-	console.log('inside directive');
-	return {
-		restrict: 'E',
-		templateUrl: './app/signin/signin.html',
-		controller: 'SigninController'
-	};
 });
--- client/app/signup/signup.js
+++ client/app/signup/signup.js
@@ -2,11 +2,4 @@ angular.module('app.signup', [])
 
 .controller('SignupController', function ($scope, $http, $location) {
   
-}).directive('signUp', function() {
-	console.log('inside directive');
-	return {
-		restrict: 'E',
-		templateUrl: './app/signup/signup.html',
-		controller: 'SignupController'
-	};
 });
